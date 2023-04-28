dff72d06bf4269f29dfeb9a2214c52bd9cad4e93
Andrei <andrei.carip@gmail.com>
2023-04-28 14:25:08 +0300

    am scris catar e smecher

--- run.py
+++ run.py
@@ -11,6 +11,7 @@ def run():
 
     if run_type == 0:
         team, points = simulate_championship(teams)
+        print("Catar e smecher")
 
     if run_type == 1:
         for i in range(0, 10000):
